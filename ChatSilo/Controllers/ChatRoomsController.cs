65484e44eac5b29083d614a8c47f349e52774468
Diego Antonio Belardinelli <belardinelli.diego@gmail.com>
2023-12-03 18:27:25 +0100

    Users and Chats Controllers

---
+++ ChatSilo/Controllers/ChatRoomsController.cs
@@ -0,0 +1,22 @@
+﻿using GrainInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatSilo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatRoomsController : ControllerBase
+    {
+
+        private readonly IGrainFactory _grainFactory;
+
+        public ChatRoomsController(IGrainFactory grainFactory)
+        {
+            _grainFactory = grainFactory;
+        }
+
+
+
+    }
+}
---
+++ ChatSilo/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using GrainInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatSilo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        //private readonly IUser _user;
+
+        private readonly IGrainFactory _grainFactory;
+
+
+        /*public UsersController(IUser user)
+        {
+            _user = user;  
+        }*/
+
+        public UsersController(IGrainFactory grainFactory)
+        {
+            _grainFactory = grainFactory;
+        }
+
+
+    }
+}
